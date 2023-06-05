b589d4426d6af96f490ca68b0d3cfa728dc7069d
Kyle Weems <weemsquirrel@gmail.com>
2023-06-05 16:31:13 -0700

    Transcribe update removing axios.

--- src/api/transcribe.ts
+++ src/api/transcribe.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export type TranscribeApiEndpointOptions = {
   language?: string;
   modelSize?: string;
@@ -9,22 +7,25 @@ export const transcribe = async (
   blob: Blob,
   options?: TranscribeApiEndpointOptions
 ): Promise<string> => {
-  const headers = {
-    "content-type": "multipart/form-data",
-  };
   const language = options?.language || "english";
   const modelSize = options?.modelSize || "base";
   const formData = new FormData();
   formData.append("language", language);
   formData.append("model_size", modelSize); // options are "tiny", "base", "medium" (no english models for "large", "large-v1")
   formData.append("audio_data", blob, `tr_${new Date().getTime()}`); // "temp_recording");
+
   try {
-    const response = await axios.post(
-      "http://127.0.0.1:8000/transcribe",
-      formData,
-      { headers }
-    );
-    return response.data as string;
+    const response = await fetch("http://127.0.0.1:8000/transcribe", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.text();
+    return data as string;
   } catch (error) {
     // sometimes the transcription fails on the server, so we'll just return an empty string to avoid breaking the app
     return "";
